0b8fe8750c1e74f705113c98e80899bfba3f6e24
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-20 16:24:48 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Laravel</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li>
+                    <a href="">Duck</a>
+                </li>
+                <li>
+                    <a href="">Cat</a>
+                </li>
+                <li>
+                    <a href="">Flayer</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <main>        
+        <h1>
+            Hello World!
+        </h1>
+    </main>
+</body>
+
+<style>
+    /* body{
+        background-color: blueviolet;
+    } */
+</style>
+</html>
